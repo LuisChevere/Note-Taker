09670b15f7df8af03b5ebda50a478dc3a2c459ed
Luis <Hairchevere@gmail.com>
2022-12-28 14:06:58 -0600

    added require for routes and listen for PORT

--- develop/server.js
+++ develop/server.js
@@ -7,3 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+require('./routes/routes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})

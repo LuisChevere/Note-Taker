5480f37c491697fd170fdda9347baf72077583ee
Luis <Hairchevere@gmail.com>
2022-12-28 12:39:14 -0600

    added get notes function and update DB function

--- develop/routes/routes.js
+++ develop/routes/routes.js
@@ -21,12 +21,21 @@ module.exports = app => {
             res.JSON(notes[req.params.id]);
         });
 
-        app.delete("/api/notes/:id", function(req,res){
+        app.delete("/api/notes/:id", function(req,res) {
             notes.splice(req.params.id, 1);
             updateDB();
             console.log("Note deleted, id "+req.params.id);
         });
 
-        
-    })
+        app.get('/notes', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+
+        function updateDB() {
+            fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+    });
 }

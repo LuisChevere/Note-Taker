1255c59dbcbdfe70ae10f43b7b433b98b7eb1621
Luis <Hairchevere@gmail.com>
2022-12-28 12:34:21 -0600

    added get & delete functions

--- develop/routes/routes.js
+++ develop/routes/routes.js
@@ -10,6 +10,23 @@ module.exports = app => {
             res.JSON(notes);
         });
 
+        app.post("/api/notes", function(req,res) {
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDB();
+            return console.log("New note added: "+newNote.title);
+        });
+
+        app.get("/api/notes/:id", function(req,res) {
+            res.JSON(notes[req.params.id]);
+        });
+
+        app.delete("/api/notes/:id", function(req,res){
+            notes.splice(req.params.id, 1);
+            updateDB();
+            console.log("Note deleted, id "+req.params.id);
+        });
+
         
     })
 }

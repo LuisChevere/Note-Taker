e04a54237c8f857c1b74c6ea073dbb02b290b157
Luis <Hairchevere@gmail.com>
2022-12-28 12:28:10 -0600

    start of module export function readfile

---
+++ develop/routes/routes.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = app => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        var notes = JSON.parse(data);
+
+        app.get("/api/notes/", function(res,req) {
+            res.JSON(notes);
+        });
+
+        
+    })
+}

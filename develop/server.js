16b5ffd704007cc3129dd741179dbd298ce723f3
Luis <Hairchevere@gmail.com>
2022-12-28 19:38:18 -0600

    adding routes to server to delete routes folder

--- develop/server.js
+++ develop/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allNotes = require("./db/db.json");
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname));

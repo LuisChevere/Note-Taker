602e676ef74896a26824a0a0206d8bc7cac7a804
Luis <Hairchevere@gmail.com>
2022-12-29 10:55:03 -0600

    added send file to notes.html

--- develop/server.js
+++ develop/server.js
@@ -13,7 +13,12 @@ app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-})
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
